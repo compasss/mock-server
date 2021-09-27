d1a22cdc425a446f9b561bb64eeb7b7c53bbfe80
compasss <crazymingzai@gmail.com>
2021-09-27 18:26:53 +0800

    mock数据

--- routes/hsw/index.js
+++ routes/hsw/index.js
@@ -4,13 +4,12 @@ const template = require('./template')
 module.exports = async function (fastify, opts, next) {
   fastify.get('/manage/type/list', async function (request, reply) {
     let page = parseInt(request.query.page || 1);
-    let rows = parseInt(request.query.rows || 10);
     let totalPage = 3;  // 总共的页数
     let key = page === totalPage ? 'data|1-5' : 'data|10';
     let _template = {
       totalRecords: 25
     };
-    _template[key] = [template.list]
+    _template[key] = [template.manageTypeList]
     let data = Mock.mock(_template)
     reply.send(data)
   })
@@ -20,6 +19,51 @@ module.exports = async function (fastify, opts, next) {
   })
   fastify.post('/manage/type/delete', async function(request, reply) {
     reply.send('success')
+  })
+
+  fastify.get('/road/list', async function (request, reply) {
+    let page = parseInt(request.query.page || 1);
+    let totalPage = 3;  // 总共的页数
+    let key = page === totalPage ? 'data|1-5' : 'data|10';
+    let _template = {
+      totalRecords: 25
+    };
+    _template[key] = [template.roadList]
+    let data = Mock.mock(_template)
+    reply.send(data)
+  })
+  fastify.get('/node/list', async function (request, reply) {
+    let page = parseInt(request.query.page || 1);
+    let totalPage = 3;  // 总共的页数
+    let key = page === totalPage ? 'data|1-5' : 'data|10';
+    let _template = {
+      totalRecords: 25
+    };
+    _template[key] = [template.nodeList]
+    let data = Mock.mock(_template)
+    reply.send(data)
+  })
+  fastify.get('/seg/list', async function (request, reply) {
+    let page = parseInt(request.query.page || 1);
+    let totalPage = 3;  // 总共的页数
+    let key = page === totalPage ? 'data|1-5' : 'data|10';
+    let _template = {
+      totalRecords: 25
+    };
+    _template[key] = [template.segList]
+    let data = Mock.mock(_template)
+    reply.send(data)
+  })
+  fastify.get('/device/list', async function (request, reply) {
+    let page = parseInt(request.query.page || 1);
+    let totalPage = 3;  // 总共的页数
+    let key = page === totalPage ? 'data|1-5' : 'data|10';
+    let _template = {
+      totalRecords: 25
+    };
+    _template[key] = [template.deviceList]
+    let data = Mock.mock(_template)
+    reply.send(data)
   })
 }
 
--- routes/hsw/template.js
+++ routes/hsw/template.js
@@ -1,5 +1,5 @@
 const Mock = require('mockjs')
-module.exports.list = {
+module.exports.manageTypeList = {
   'uuid|+1': 1,
   'reason': function() {
     return Mock.Random.csentence(5, 15);
@@ -10,4 +10,57 @@ module.exports.list = {
   'opTime': function() {
     return Mock.Random.date('yyyy-MM-dd HH:mm:ss')
   }
+}
+
+module.exports.roadList = {
+  'uuid|+1': 1,
+  'code|+1': 1,
+  'name': function() {
+    return Mock.Random.csentence(3, 5);
+  },
+  'enable': true
+}
+
+module.exports.nodeList = {
+  'uuid|+1': 1,
+  'code|+1': 1,
+  'name': function() {
+    return Mock.Random.csentence(3, 5);
+  },
+  'longitudeAmap|100-116.5': '112',
+  'latitudeAmap|28-29.5': '12',
+  'stakeNo': '桩号',
+  'roadName': function() {
+    return Mock.Random.csentence(3, 5);
+  },
+  'enabled': true
+}
+
+module.exports.segList = {
+  'uuid|+1': 1,
+  'segName': function() {
+    return Mock.Random.csentence(3, 5);
+  },
+  'startPosition': '1.00',
+  'endPosition': '2.00',
+  'kilo': '里程',
+  'segTypeCode': 'fefe',
+  'laneNum': 2,
+  'enabled': true
+}
+
+module.exports.deviceList = {
+  'uuid|+1': 1,
+  'name': function() {
+    return Mock.Random.csentence(3, 5);
+  },
+  'typeCode': '123',
+  'ioTypeCode': '2.00',
+  'location': '位置',
+  'direction': 'west',
+  'ip': '192.168.1.1',
+  'port': '9000',
+  'user': 'user',
+  'password': 'password',
+  'enabled': true
 }
